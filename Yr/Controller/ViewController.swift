d344b7afb8bb132c2a8e21ffebc4466ce5a8bfb1
Dzmitry <skysilvaby@gmail.com>
2020-12-17 00:19:25 +0300

    Update ViewController

--- Yr/Controller/ViewController.swift
+++ Yr/Controller/ViewController.swift
@@ -8,8 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: - Properties
+
+    private var airTemperature = "--"
+    private var airTemperatureLabel: UILabel?
+    private var dataSource: WeatherDataSource?
+    private var weather = [Weather]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
+        airTemperatureLabel = UILabel(frame: CGRect(x: 100, y: 100, width: 200.00, height: 40.00))
+        if let airTemperatureLabel = airTemperatureLabel {
+            view.addSubview(airTemperatureLabel)
+            airTemperatureLabel.text = "--"
+            airTemperatureLabel.textAlignment = .center
+        }
+        requestWeatherForLocation()
+    }
+
+    override func viewWillAppear(_: Bool) {
+        super.viewWillAppear(true)
+        requestWeatherForLocation()
+    }
+
+    func requestWeatherForLocation() {
+        dataSource = WeatherDataSource { [weak self] data in
+            self?.weather = data
+
+            if let airTemperature = self?.weather.first?.properties.timeseries.first?.data.instant.details.airTemperature {
+                self?.airTemperature = String(airTemperature)
+            }
+            DispatchQueue.main.async {
+                self?.airTemperatureLabel?.text = "\(self!.airTemperature) \u{2103}"
+            }
+        }
     }
 }
